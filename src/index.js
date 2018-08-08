bd95138be666c1a311069da13795310bfd8e7ffc
Ram <ramkrishna.register@gmail.com>
2018-08-08 13:04:31 +0530

    plotted line and points in svg

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 const keys = Object.keys(airlines);
 var dataPoints = new Array(24);
-
+var selectedAirline = 'total';
 var initialCounterObj = {};
 keys.map(function (key) {
   initialCounterObj[key] = 0;
 });
+
 initialCounterObj["total"] = 0;
 initDataPoints(dataPoints);
 flights_jan_01_2008.map(function (data) {
@@ -23,8 +24,6 @@ flights_jan_01_2008.map(function (data) {
   }
 });
 
-console.log(dataPoints);
-
 function initDataPoints(dataPoints) {
   for (var i = 0; i < dataPoints.length; i++) {
     dataPoints[i] = Object.assign({}, initialCounterObj);
@@ -39,7 +38,12 @@ function recalculateChartSize() {
   var headerHeight = document.getElementById('header').clientHeight;
   var windowHeight = window.innerHeight;
   var availableHeight = windowHeight - headerHeight - Math.ceil(windowHeight / 5);
-  document.getElementById('chart-container').setAttribute("style", `height:${availableHeight}px`);
+  var chartContainer = document.getElementById('chart-container');
+  chartContainer.setAttribute("style", "height:" + availableHeight + "px");
+  var chartWidth = chartContainer.clientWidth;
+  var chartHeaderHeight = document.getElementById('chart-header').clientHeight;
+  var bodyLeftMargin = window.getComputedStyle(document.body).marginLeft;
+  plotGraph(dataPoints, selectedAirline, chartWidth, availableHeight - chartHeaderHeight, parseInt(bodyLeftMargin) + 10, 0);
 }
 
 window.onload = initChart();
@@ -53,3 +57,80 @@ window.onresize = function () {
     }, 300);
   }
 };
+
+//SVG graph related functions
+function getCountsArray(graphData, attribute) {
+  return graphData.map(function (item) {
+    return item[attribute];
+  });
+}
+
+function calculateSVGData(graphData, attribute, width, height, xoffset, yoffset) {
+  var values = getCountsArray(graphData, attribute);
+  return getCoordinates(values, width, height, xoffset, yoffset)
+}
+
+
+function getCoordinates(values, width, height, xoffset, yoffset) {
+  var min = Math.floor(Math.min.apply(null, values) * 0.8);
+  var max = Math.ceil(Math.max.apply(null, values) * 1.2);
+
+  var yRatio = (max - min) / height;
+  var xRatio = width / (values.length);
+  return values.map(function (value, i) {
+    var y = height - ((value - min) / yRatio);
+    var x = (xRatio * i) - (xRatio / 2);
+    return [x + xoffset, y + yoffset];
+  });
+}
+
+function plotGraph(data, attribute, width, height, xoffset = 0, yoffset = 0) {
+  var svgData = calculateSVGData(data, attribute, width, height, xoffset, yoffset);
+  var svgChart = document.getElementById('svg-chart');
+  svgChart.innerHTML = "";
+  svgChart.setAttribute("style", "shape-rendering:auto;height:" + height + "px; width:" + width + "px");
+
+  drawLine(svgChart, svgData);
+  drawPoints(svgChart, svgData);
+}
+
+function drawLine(svgElement, svgData) {
+  var lineData = "";
+  svgData.map(function (coordinates, i) {
+    var command = i === 0 ? "M" : "L";
+    lineData = lineData
+      + " "
+      + command
+      + " "
+      + coordinates[0]
+      + ","
+      + coordinates[1]
+  });
+
+  var line = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "path"
+  );
+  line.setAttribute("d", lineData);
+  line.setAttribute("fill", "none");
+  line.setAttribute("stroke", "#5CC0C0");
+  line.setAttribute("stroke-width", 3);
+  svgElement.appendChild(line);
+}
+
+function drawPoints(svgElement, svgData) {
+  svgData.map(function (coordinates) {
+    var point = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "circle"
+    );
+
+    point.setAttribute("cx", coordinates[0]);
+    point.setAttribute("cy", coordinates[1]);
+    point.setAttribute("r", 4);
+    point.setAttribute("fill", "#5CC0C0");
+    point.setAttribute("stroke", "#fff");
+    point.setAttribute("stroke-width", 2);
+    svgElement.appendChild(point);
+  });
+}
