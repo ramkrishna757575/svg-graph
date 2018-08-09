10b49454c2a60eaa3ab687443e081b2685688728
Ram <ramkrishna.register@gmail.com>
2018-08-09 12:29:39 +0530

    added comments

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
+//some constants to be used for plotting the graph
 const keys = Object.keys(airlines);
 const HOURS = 24;
-const ALL_AIRLINES_KEY = 'total';
-const ALL_AIRLINES_VALUE = 'All Airlines';
-var dataPoints = new Array(HOURS);
-var selectedAirline = ALL_AIRLINES_KEY;
+const ALL_AIRLINES_KEY = 'total';           //key to be used when showing all airlines graph
+const ALL_AIRLINES_VALUE = 'All Airlines';  //value to be used when showing all airlines graph
+var dataPoints = new Array(HOURS);          //An array to hold count of airlines per hour of a day
+var selectedAirline = ALL_AIRLINES_KEY;     //holds the current selection of airline for plotting its data
+
+//initialise an empty object and fill it with keys from the airlines object
+//will hold count of each airlines per hour
 var initialCounterObj = {};
 keys.map(function (key) {
   initialCounterObj[key] = 0;
 });
 
+//adding the total count of all airlines per hour in initialCounterObj object
 initialCounterObj[ALL_AIRLINES_KEY] = 0;
+
+//initialising the dataPoints array to fill each entry with initialCounterObj object
 initDataPoints(dataPoints);
+
+//going through the flights data and filtering invalid entries. Then counting each airlines per hour of day
+//each airlines flight data is being segregated into 2 buckets corrsponding to each hour of the day based on
+//the time field in the data
 flights_jan_01_2008.map(function (data) {
   if (!!data) {
     var dataKeys = Object.keys(data);
@@ -27,17 +38,41 @@ flights_jan_01_2008.map(function (data) {
   }
 });
 
+/**
+ * @desc initialises an array of data points to be used to plot the graph
+ * @param dataPoints
+ */
 function initDataPoints(dataPoints) {
   for (var i = 0; i < dataPoints.length; i++) {
     dataPoints[i] = Object.assign({}, initialCounterObj);
   }
 }
 
+//when page is loaded, process and render graph
+window.onload = initPage;
+//when page is resized, rerender graph
+window.onresize = function () {
+  var resizeTimeout;
+  if (!resizeTimeout) {
+    resizeTimeout = setTimeout(function () {
+      resizeTimeout = null;
+      drawGraph();
+    }, 300);
+  }
+};
+
+/**
+ * initial setup and rendering of the graph
+ */
 function initPage() {
   drawGraph();
   setGraphHeaders();
 }
 
+/**
+ * This function calculates the available space for the svg graph
+ * and then plots it based on selected airline
+ */
 function drawGraph() {
   var headerHeight = document.getElementById('header').clientHeight;
   var windowHeight = window.innerHeight;
@@ -50,31 +85,45 @@ function drawGraph() {
   plotGraph(dataPoints, selectedAirline, chartWidth, availableHeight - chartHeaderHeight, parseInt(bodyLeftMargin) + 10, 0);
 }
 
-window.onload = initPage;
-
-window.onresize = function () {
-  var resizeTimeout;
-  if (!resizeTimeout) {
-    resizeTimeout = setTimeout(function () {
-      resizeTimeout = null;
-      drawGraph();
-    }, 300);
-  }
-};
-
-//SVG graph related functions
+/**
+ * Extracts the count data of selected airline
+ * @param graphData - the processed data
+ * @param attribute - the selected airline's key
+ * @returns {*} - array of the count of airline per hour of day
+ */
 function getCountsArray(graphData, attribute) {
   return graphData.map(function (item) {
     return item[attribute];
   });
 }
 
+/**
+ * calculates the svg coordinate data to be used to render the svg graph
+ * @param graphData - the processed graph data
+ * @param attribute - the selected airline's key
+ * @param width - the available width for the graph
+ * @param height - the available height for the graph
+ * @param xOffset - any offset to be used on the x-axis of the graph
+ * @param yOffset - any offset to be used on the y-axis of the graph
+ * @returns {{coordinates, distance}} - object containing array of the point coordinates
+ * and interval on x-axis in pixels
+ */
 function calculateSVGData(graphData, attribute, width, height, xOffset = 0, yOffset = 0) {
   var values = getCountsArray(graphData, attribute);
   return getCoordinates(values, width, height, xOffset, yOffset)
 }
 
-
+/**
+ * calculates the svg coordinate data for an array of values(an airline/all airlines)
+ * to be used to render the svg graph
+ * @param values - count of the airlines per hour
+ * @param width - the available width for the graph
+ * @param height - the available height for the graph
+ * @param xOffset - any offset to be used on the x-axis of the graph
+ * @param yOffset - any offset to be used on the y-axis of the graph
+ * @returns {{coordinates: *, distance: number}} - object containing array of the point coordinates
+ * and interval on x-axis in pixels
+ */
 function getCoordinates(values, width, height, xOffset = 0, yOffset = 0) {
   var min = Math.floor(Math.min.apply(null, values) * 0.8);
   var max = Math.ceil(Math.max.apply(null, values) * 1.2);
@@ -92,6 +141,15 @@ function getCoordinates(values, width, height, xOffset = 0, yOffset = 0) {
   };
 }
 
+/**
+ * draws all the elements of the svg to render the graph
+ * @param data - the processed airlines data
+ * @param attribute - the selected airline's key
+ * @param width - the available width for the graph
+ * @param height - the available height for the graph
+ * @param xOffset - any offset to be used on the x-axis of the graph
+ * @param yOffset - any offset to be used on the y-axis of the graph
+ */
 function plotGraph(data, attribute, width, height, xOffset = 0, yOffset = 0) {
   var calculatedData = calculateSVGData(data, attribute, width, height, xOffset, yOffset);
   var svgData = calculatedData.coordinates;
@@ -111,6 +169,11 @@ function plotGraph(data, attribute, width, height, xOffset = 0, yOffset = 0) {
   addHoverListeners();
 }
 
+/**
+ * takes the generated svg coordinates and generates a command to draw svg path
+ * @param svgData
+ * @returns {string}
+ */
 function getLineCommand(svgData) {
   var lineData = "";
   svgData.map(function (coordinates, i) {
@@ -126,6 +189,11 @@ function getLineCommand(svgData) {
   return lineData;
 }
 
+/**
+ * creates a path in the specified svg element based on the generated svg coordinates
+ * @param svgElement
+ * @param svgData
+ */
 function drawLine(svgElement, svgData) {
   var lineData = getLineCommand(svgData);
   var line = document.createElementNS(
@@ -139,6 +207,11 @@ function drawLine(svgElement, svgData) {
   svgElement.appendChild(line);
 }
 
+/**
+ * creates data points in the specified svg element based on the generated svg coordinates
+ * @param svgElement
+ * @param svgData
+ */
 function drawPoints(svgElement, svgData) {
   svgData.map(function (coordinates) {
     var point = document.createElementNS(
@@ -156,6 +229,13 @@ function drawPoints(svgElement, svgData) {
   });
 }
 
+/**
+ * shades the area covered by the graph plotted
+ * @param svgElement
+ * @param svgData
+ * @param height
+ * @param xDistance
+ */
 function drawArea(svgElement, svgData, height, xDistance) {
   var areaPoints = getLineCommand(svgData);
   areaPoints = areaPoints +
@@ -175,6 +255,13 @@ function drawArea(svgElement, svgData, height, xDistance) {
   svgElement.appendChild(area);
 }
 
+/**
+ * draws the vertical grid lines on the graph showing the 24 hours intervals
+ * @param svgElement
+ * @param dataCount
+ * @param xDistance
+ * @param height
+ */
 function drawGridLines(svgElement, dataCount, xDistance, height) {
   var lineData = "";
   for (var i = 1; i < dataCount; i++) {
@@ -192,6 +279,14 @@ function drawGridLines(svgElement, dataCount, xDistance, height) {
   svgElement.appendChild(line);
 }
 
+/**
+ * draws invisible rectangles in each inteval.
+ * Used to listen to mouse-hover event in each interval
+ * @param svgElement
+ * @param dataCount
+ * @param xDistance
+ * @param height
+ */
 function drawTransparentIntervalRects(svgElement, dataCount, xDistance, height) {
   for (var i = 1; i <= dataCount; i++) {
     var rect = document.createElementNS(
@@ -208,6 +303,13 @@ function drawTransparentIntervalRects(svgElement, dataCount, xDistance, height) 
   }
 }
 
+/**
+ * highlights the area under the graph in the interval currently in focus
+ * @param svgElement
+ * @param dataCount
+ * @param xDistance
+ * @param height
+ */
 function drawSelectionForeground(svgElement, dataCount, xDistance, height) {
   for (var i = 1; i <= dataCount; i++) {
     var rect = document.createElementNS(
@@ -226,6 +328,13 @@ function drawSelectionForeground(svgElement, dataCount, xDistance, height) {
   }
 }
 
+/**
+ * highlights the area above the graph in the interval currently in focus
+ * @param svgElement
+ * @param dataCount
+ * @param xDistance
+ * @param height
+ */
 function drawSelectionBackground(svgElement, dataCount, xDistance, height) {
   for (var i = 1; i <= dataCount; i++) {
     var rect = document.createElementNS(
@@ -243,6 +352,13 @@ function drawSelectionBackground(svgElement, dataCount, xDistance, height) {
   }
 }
 
+/**
+ * clips the highlighted selection rectangle(in the currently focused interval)
+ * @param svgElement
+ * @param svgData
+ * @param xDistance
+ * @param height
+ */
 function drawClipPath(svgElement, svgData, xDistance, height) {
   var clipPathElement = document.createElementNS(
     "http://www.w3.org/2000/svg",
@@ -267,6 +383,13 @@ function drawClipPath(svgElement, svgData, xDistance, height) {
   clipPathElement.appendChild(clipPath);
 }
 
+/**
+ * renders the value of the points in the graph
+ * @param svgElement
+ * @param svgData
+ * @param data
+ * @param attribute
+ */
 function drawValues(svgElement, svgData, data, attribute) {
   for (var i = 0; i < data.length; i++) {
     var value = document.createElementNS(
@@ -283,6 +406,12 @@ function drawValues(svgElement, svgData, data, attribute) {
   }
 }
 
+/**
+ * returns the index of the currently focused interval
+ * @param node
+ * @param myClass
+ * @returns {number}
+ */
 function indexInClass(node, myClass) {
   var className = node.className;
   var num = 0;
@@ -296,6 +425,10 @@ function indexInClass(node, myClass) {
   return -1;
 }
 
+/**
+ * attaches mouseover and mouseout event listeners to the transparent rectangle elements in
+ * each interval. Used to show/hide selected/unselected interval
+ */
 function addHoverListeners() {
   var hoverAreas = document.getElementsByClassName("hoverArea");
   for (var i = 0; i < hoverAreas.length; i++) {
@@ -308,6 +441,10 @@ function addHoverListeners() {
   }
 }
 
+/**
+ * hides the selection components of the interval at index i
+ * @param i - interval index
+ */
 function removeSelection(i) {
   var selectionBackgrounds = document.getElementsByClassName("selectionBackground");
   var selectionForegrounds = document.getElementsByClassName("selectionForeground");
@@ -317,6 +454,10 @@ function removeSelection(i) {
   selectedValues[i].style.display = 'none';
 }
 
+/**
+ * shows the selection components of the interval at index i
+ * @param i - interval index
+ */
 function setSelection(i) {
   var selectionBackgrounds = document.getElementsByClassName("selectionBackground");
   var selectionForegrounds = document.getElementsByClassName("selectionForeground");
@@ -326,6 +467,9 @@ function setSelection(i) {
   selectedValues[i].style.display = 'block';
 }
 
+/**
+ * sets the graph headers showing the flights count, airline name
+ */
 function setGraphHeaders() {
   var flightsCountElement = document.getElementById("flights-count");
   var flightsCount = 0;
@@ -338,6 +482,10 @@ function setGraphHeaders() {
   airlineNameElement.innerText = airlines[selectedAirline] || ALL_AIRLINES_VALUE;
 }
 
+/**
+ * re-renders graph when airline selection changes
+ * @param e
+ */
 function updateGraphOnInputChange(e) {
   if (e.type === 'keypress' && e.key === 'Enter' || e.type === 'blur') {
     if (!!e.target) {
@@ -359,6 +507,9 @@ function updateGraphOnInputChange(e) {
   }
 }
 
+/**
+ * resets and shows the graph for all airlines
+ */
 function showAllAirlinesGraph() {
   selectedAirline = ALL_AIRLINES_KEY;
   drawGraph();
